98f5d938df4846aed0b2d4a794c6e1d2a68a4e43
ant-mad <0.0.mad.ant@gmail.com>
2019-02-19 21:44:39 +0300

    Latest memory changes

--- contracts/test_1_snax.system/test_1_snax.system.cpp
+++ contracts/test_1_snax.system/test_1_snax.system.cpp
@@ -23,14 +23,14 @@ namespace snaxsystem {
       auto itr = _rammarket.find(S(4,RAMCORE));
 
       if( itr == _rammarket.end() ) {
-         auto system_token_supply   = snax::token(N(snax.token)).get_supply(snax::symbol_type(system_token_symbol).name()).amount;
+         const int64_t system_token_supply = snax::token(N(snax.token)).get_supply(snax::symbol_type(system_token_symbol).name()).amount;
          if( system_token_supply > 0 ) {
             itr = _rammarket.emplace( _self, [&]( auto& m ) {
                m.supply.amount = 100000000000000ll;
                m.supply.symbol = S(4,RAMCORE);
                m.base.balance.amount = int64_t(_gstate.free_ram());
                m.base.balance.symbol = S(0,RAM);
-               m.quote.balance.amount = system_token_supply / 10;
+               m.quote.balance.amount = system_token_supply / 1000;
                m.quote.balance.symbol = CORE_SYMBOL;
             });
          }
