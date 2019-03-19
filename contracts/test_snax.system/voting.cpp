61fb43da8f9e8cad203ac4dfb90dcf08e382141b
ant-mad <0.0.mad.ant@gmail.com>
2019-03-19 12:14:56 +0000

    Merge branch 'master' into 'last_block_time_to_zero'
    # Conflicts:
    #   contracts/snax.system/voting.cpp
    #   contracts/test_snax.system/voting.cpp

--- contracts/test_snax.system/voting.cpp
+++ contracts/test_snax.system/voting.cpp
@@ -40,6 +40,8 @@ namespace snaxsystem {
       require_auth( producer );
 
       auto prod = _producers.find( producer );
+      
+      const auto current_time = snax::time_point_sec(now());
 
       if ( prod != _producers.end() ) {
          _producers.modify( prod, producer, [&]( producer_info& info ){
@@ -47,7 +49,7 @@ namespace snaxsystem {
                info.is_active    = true;
                info.url          = url;
                info.location     = location;
-               info.last_block_time = block_timestamp(0);
+               info.last_block_time = block_timestamp(current_time);
             });
       } else {
          _producers.emplace( producer, [&]( producer_info& info ){
@@ -57,7 +59,7 @@ namespace snaxsystem {
                info.is_active     = true;
                info.url           = url;
                info.location      = location;
-               info.last_block_time = block_timestamp(0);
+               info.last_block_time = block_timestamp(current_time);
          });
       }
    }
